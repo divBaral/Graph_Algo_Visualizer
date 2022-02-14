33308035a48e9cf7d7f3433f04af8a1577b14b60
Sagar-Kr-Thapa <076bct062.sagar@pcampus.edu.np>
2022-02-15 01:05:11 +0545

    Build directory cleaned

--- include/Queue.h
+++ include/Queue.h
@@ -16,28 +16,29 @@ public:
     bool empty(){return Front==nullptr; }
 	void enqueue( Vertex *x )
 	{
-                node *newNode = new node;
-                newNode->data = x;
+        node *newNode = new node;
+        newNode->data = x;
 
-                if( Front == nullptr )
-                        Front = Rear = newNode;
-                else
-                {
-                        Rear->next = newNode;
-                        Rear = newNode;
-                }
+        if( Front == nullptr )
+            Front = Rear = newNode;
+        else
+        {
+            Rear->next = newNode;
+            Rear = newNode;
+        }
 	}
 
 	Vertex* dequeue()
 	{
-                if( Front != nullptr )
-                {
-                        Vertex *info = Front->data;
-                        node *tempNode = Front;
-                        Front = Front->next;
-                        delete tempNode;
+        if( Front != nullptr )
+        {
+            Vertex *info = Front->data;
+            node *tempNode = Front;
+            Front = Front->next;
+            delete tempNode;
 
-                        return info;
-                }
+            return info;
+        }
+            return NULL;
 	}
 };
